e6140d2f99075a7973dbd342747650c5b141aff4
Sarup Banskota <sbanskota08@gmail.com>
2014-12-06 21:50:17 +0530

    Add rails skeleton, start notes locally

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Koding::Application.config.secret_key_base = 'd5e069cd5bc04dafecf2cf5958f23712d3f0554337bf7e20c64333a93ca2c02ede41bf0437f3cdebc4d1514ef39c5992e572e89498fb00acdd03e5f0b4148a0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Koding::Application.config.session_store :cookie_store, key: '_koding_session'
