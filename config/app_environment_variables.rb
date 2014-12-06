e0927fd51a704d56b369e60005817a29ea11e615
Sarup Banskota <sbanskota08@gmail.com>
2014-12-06 22:59:25 +0530

    Add devise + set up mailing

---
+++ config/app_environment_variables.rb
@@ -0,0 +1,3 @@
+ENV["GMAIL_DOMAIN"] = "gmail.com"
+ENV["GMAIL_USERNAME"] = "sbanskota08@gmail.com"
+ENV["GMAIL_PASSWORD"] = "scratchpadsarenotparallel"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,19 @@ Koding::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
 end
