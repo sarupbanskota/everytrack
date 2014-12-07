8302ece3dabb25b5356e0765cb3069cb887655da
Sarup Banskota <sbanskota08@gmail.com>
2014-12-07 20:14:25 +0530

    Refactor banners and sign out links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Koding::Application.routes.draw do
   resources :units
 
-  devise_for :users
+  devise_for :users do
+  	get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :tracks
   
   root to: "tracks#index"
